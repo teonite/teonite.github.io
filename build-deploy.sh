6a12bec1430254cf84ff88f405f23d11617266c8
Robert Olejnik <robert@teonite.com>
2024-02-16 12:12:30 +0100

    build & deploy using docker

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-pnpm build && (cd dist && bsdtar --no-acls --no-mac-metadata --no-xattrs --exclude Dockerfile --exclude-vcs --exclude '*.yml' --exclude README.md --exclude '*.sh' -cf - . | kubectl -n dynamic-sites exec -it deploy/dynamic-sites -- tar -C /sites/teonite.com -xf -)
+#pnpm build && (cd dist && bsdtar --no-acls --no-mac-metadata --no-xattrs --exclude Dockerfile --exclude-vcs --exclude '*.yml' --exclude README.md --exclude '*.sh' -cf - . | kubectl -n dynamic-sites exec -it deploy/dynamic-sites -- tar -C /sites/teonite.com -xf -)
+echo building using docker
+docker-compose -f docker-compose.build.yaml up
+echo deploying
+cd docker-dist && bsdtar --no-acls --no-mac-metadata --no-xattrs --exclude Dockerfile --exclude-vcs --exclude '*.yml' --exclude README.md --exclude '*.sh' -cf - . | kubectl -n dynamic-sites exec -it deploy/dynamic-sites -- tar -C /sites/teonite.com -xf -
