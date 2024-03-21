1f4fd5b8a69fb8b496fd9a673c6612f2d3849326
Michał Gryczka <mgryczka@teonite.com>
2024-03-21 15:00:12 +0100

    Update build-deploy.sh
    deployment update

--- build-deploy.sh
+++ build-deploy.sh
@@ -4,4 +4,4 @@
 echo building using docker
 docker-compose -f docker-compose.build.yaml up
 echo deploying
-cd docker-dist && bsdtar --no-acls --no-mac-metadata --no-xattrs --exclude Dockerfile --exclude-vcs --exclude '*.yml' --exclude README.md --exclude '*.sh' -cf - . | kubectl -n dynamic-sites exec -it deploy/dynamic-sites -- tar -C /sites/teonite.com -xf -
+cd docker-dist && bsdtar --no-acls --no-mac-metadata --no-xattrs --exclude Dockerfile --exclude-vcs --exclude '*.yml' --exclude README.md --exclude '*.sh' -cf - . | kubectl -n dynamic-sites exec -i deploy/dynamic-sites -- tar -C /sites/teonite.com -xf -
