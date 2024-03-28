c2c226782e1507ecef1f187b0146c926ed8a51a4
4LB <kk@4lb.io>
2024-03-28 16:33:10 +0100

    blog added article and style tweaks, TBC

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,5 +24,36 @@ export default defineConfig({
   integrations: [mdx()],
   markdown: {
     rehypePlugins: [rehypeFigure],
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      theme: 'github-light',
+      // Alternatively, provide multiple themes
+      // https://shiki.style/guide/dual-themes
+      // themes: {
+      //   light: 'github-light',
+      //   dark: 'github-dark',
+      // },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
   }
 });
+
+const html = await codeToHtml(
+  code,
+  {
+    lang: 'js',
+    theme: 'min-dark',
+    colorReplacements: {
+      '#fff': '#189eff'
+    }
+  }
+)
