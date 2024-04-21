1474a0545e1ca3e82efb748e35e74a82c4714132
Filip Slezak <filipslezaklab@gmail.com>
2024-04-21 17:14:03 +0200

    add fintech work group page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,36 +1,43 @@
 import { defineCollection, z } from "astro:content";
 
 const services = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        subtitle: z.string(),
-        cover: z.string(),
-    }),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string(),
+    cover: z.string(),
+  }),
 });
 
+const workCategory = z.enum(["ai", "fintech", "security"]);
+
+export type WorkCategory = z.infer<typeof workCategory>;
 
 const work = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        subtitle: z.string(),
-        cover: z.string(),
-        category: z.array(z.enum(['ai', 'fintech', 'security'])),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string(),
+    cover: z.string(),
+    category: z.array(workCategory),
+    display: z.object({
+      title: z.string(),
+      description: z.string(),
     }),
+  }),
 });
 
 const blog = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        subtitle: z.string(),
-        cover: z.string(),
-    }),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string(),
+    cover: z.string(),
+  }),
 });
 
 export const collections = {
-    services,
-    work,
-    blog
+  services,
+  work,
+  blog,
 };
