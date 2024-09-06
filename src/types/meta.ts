1aae61b7be67ec7c6a2c28e8e42c5a1a507e6ced
cpprian <cpprian456@gmail.com>
2024-09-06 21:44:42 +0200

    meta & images

--- src/types/meta.ts
+++ src/types/meta.ts
@@ -1,11 +1,13 @@
 import { z } from "astro:content";
 
-export const facebookMeta = z.object({
-    title: z.string().min(1).default("haha"),
-    description: z.string().min(1).default("Deeptech studio crafting mission-critical software"),
-    image: z.string().min(1).default("/public/images/pages/home/teonite.png"),
-    image_alt: z.string().min(1).default("teonite"),
-    imageWidth: z.string().min(3).default("1200"),
-    imageHeight: z.string().min(3).default("630"),
-});
+export const metaOGDefault = {
+    title: "teonite — deeptech studio",
+    description: "Deeptech studio crafting mission-critical software",
+    image: "github.com/teonite/teonite.github.io/raw/main/public/images/pages/home/teonite.png",
+};
 
+export const metaOG = z.object({
+    title: z.string().min(1).default(metaOGDefault.title),
+    description: z.string().min(1).default(metaOGDefault.description),
+    image: z.string().min(1).default(metaOGDefault.image),
+});
